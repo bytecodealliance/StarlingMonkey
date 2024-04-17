c6ffea7838c96bc6571e66d6c3ee15c682c2cf4a
Guy Bedford <gbedford@fastly.com>
2024-04-17 10:19:25 -0700

    fix CI (#25)

--- componentize.sh
+++ componentize.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+wizer="${WIZER:-wizer}"
+wasm_tools="${WASM_TOOLS:-wasm-tools}"
+
 # Use $2 as output file if provided, otherwise use the input base name with a .wasm extension
 if [ $# -gt 1 ]
 then
@@ -11,5 +14,5 @@ else
   OUT_FILE="${BASENAME%.*}.wasm"
 fi
 
-echo "$1" | WASMTIME_BACKTRACE_DETAILS=1 wizer --allow-wasi --wasm-bulk-memory true --inherit-stdio true --dir "$(dirname "$1")" -o "$OUT_FILE" -- "$(dirname "$0")/starling.wasm"
-wasm-tools component new -v --adapt "wasi_snapshot_preview1=$(dirname "$0")/preview1-adapter.wasm" --output "$OUT_FILE" "$OUT_FILE"
+echo "$1" | WASMTIME_BACKTRACE_DETAILS=1 $wizer --allow-wasi --wasm-bulk-memory true --inherit-stdio true --dir "$(dirname "$1")" -o "$OUT_FILE" -- "$(dirname "$0")/starling.wasm"
+$wasm_tools component new -v --adapt "wasi_snapshot_preview1=$(dirname "$0")/preview1-adapter.wasm" --output "$OUT_FILE" "$OUT_FILE"
