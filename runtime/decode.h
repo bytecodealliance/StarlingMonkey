ad8759b13e08ff20b0b76cc5640ecec7b2ec3d69
Till Schneidereit <till@tillschneidereit.net>
2024-04-30 00:02:47 +0200

    Use `string_view` instead of `HostString in core::decode`
    `HostString` converts to `string_view`, so this makes `decode` easy to use from more places.

--- runtime/decode.h
+++ runtime/decode.h
@@ -1,11 +1,9 @@
 #ifndef JS_COMPUTE_RUNTIME_DECODE_H
 #define JS_COMPUTE_RUNTIME_DECODE_H
 
-#include "host_api.h"
-
 namespace core {
 
-JSString* decode(JSContext *cx, host_api::HostString& str);
+JSString* decode(JSContext *cx, string_view str);
 
 } // namespace core
 
