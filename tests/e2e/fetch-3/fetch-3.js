98bf3308334d2b78dedb4eb6b975154ebc062d99
Nico Burniske <nicoburniske@gmail.com>
2024-04-30 14:51:14 -0400

    add post test (failing)

--- tests/e2e/fetch-3/fetch-3.js
+++ tests/e2e/fetch-3/fetch-3.js
@@ -7,13 +7,21 @@ async function main(event) {
             reject = rej;
         });
         event.respondWith(responsePromise);
+
+        const headerKey = "X-Test-Header";
+        const headerValue = "test-header-value";
         
-        let response = await fetch("https://jsonplaceholder.typicode.com/users");
-        let text = await response.json();
+        let response = await fetch("https://echo.free.beeceptor.com", {
+            method: "POST",
+            headers: {
+                [headerKey] :headerValue 
+            },
+            body: "hello world"
+        })
 
+        let responseJson = await response.json();
         console.log("Successfully received response json body");
-
-        resolve(new Response(JSON.stringify(text), { headers: response.headers }));
+        resolve(new Response(JSON.stringify(responseJson)));
     } catch (e) {
         console.log(`Error: ${e}. Stack: ${e.stack}`);
     }
