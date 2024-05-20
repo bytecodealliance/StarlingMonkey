f7aacf1ff87e4b8c805c219d0238277009865c1e
Nico Burniske <nicoburniske@gmail.com>
2024-05-20 08:43:26 -0400

    remove clean_test.sh

--- clean_test.sh
+++
@@ -1,7 +0,0 @@
-rm -rf cmake-build-debug
-
-cmake -S . -B cmake-build-debug -DCMAKE_BUILD_TYPE=Debug
-
-cmake --build cmake-build-debug --target integration-test-server
-
-CTEST_OUTPUT_ON_FAILURE=1 ctest --test-dir cmake-build-debug -j8
