d19e2611fda5e6db6c741723a3a3df9e92bdbead
Guy Bedford <gbedford@fastly.com>
2024-05-28 10:41:54 -0700

    fix: patch up build

--- cmake/spidermonkey.cmake
+++ cmake/spidermonkey.cmake
@@ -4,12 +4,18 @@ if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     set(SM_BUILD_TYPE debug)
 else()
     set(SM_BUILD_TYPE release)
+    CPMAddPackage(NAME spidermonkey-release
+        URL https://github.com/bytecodealliance/spidermonkey-wasi-embedding/releases/download/rev_${SM_REV}/spidermonkey-wasm-static-lib_release.tar.gz
+        DOWNLOAD_ONLY YES
+    )
 endif()
 
-CPMAddPackage(NAME spidermonkey-${SM_BUILD_TYPE}
-        URL https://github.com/bytecodealliance/spidermonkey-wasi-embedding/releases/download/rev_${SM_REV}/spidermonkey-wasm-static-lib_${SM_BUILD_TYPE}.tar.gz
+CPMAddPackage(NAME spidermonkey-debug
+        URL https://github.com/bytecodealliance/spidermonkey-wasi-embedding/releases/download/rev_${SM_REV}/spidermonkey-wasm-static-lib_debug.tar.gz
         DOWNLOAD_ONLY YES
 )
+
+file(COPY ${CPM_PACKAGE_spidermonkey-debug_SOURCE_DIR}/lib/libjsrust.a DESTINATION "${CPM_PACKAGE_spidermonkey-release_SOURCE_DIR}/lib")
 
 set(SM_SOURCE_DIR ${CPM_PACKAGE_spidermonkey-${SM_BUILD_TYPE}_SOURCE_DIR} CACHE STRING "Path to spidermonkey ${SM_BUILD_TYPE} build" FORCE)
 set(SM_INCLUDE_DIR ${SM_SOURCE_DIR}/include)
