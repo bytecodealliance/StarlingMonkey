cf8a8f5e292fb31174ebd34285ffd5391c6e82f5
Till Schneidereit <till@tillschneidereit.net>
2024-08-04 12:35:55 +0200

    Update various tools
    This commit includes updates of various tools used for building and testing StarlingMonkey:
    - Binaryen
    - Corrosion CMake Cargo integration
    - wasm-tools
    - Wasmtime
    - Wizer
    
    No changes in behavior should occur as a result of these updates.

--- cmake/init-corrosion.cmake
+++ cmake/init-corrosion.cmake
@@ -2,14 +2,25 @@
 # NOTE: This file must not be called `corrosion.cmake`, because otherwise it'll be called recursively instead of the
 # same-named one coming with Corrosion.
 
-set(Rust_CARGO_TARGET wasm32-wasip1)
 # Necessary to make cross-compiling to wasm32-wasi work for crates with -sys dependencies.
 set(Rust_CARGO_TARGET_LINK_NATIVE_LIBS "")
 
-file(STRINGS "${CMAKE_CURRENT_SOURCE_DIR}/rust-toolchain.toml" Rust_TOOLCHAIN REGEX "^channel ?=")
-string(REGEX MATCH "[0-9.]+" Rust_TOOLCHAIN "${Rust_TOOLCHAIN}")
-execute_process(COMMAND rustup toolchain install ${Rust_TOOLCHAIN})
-execute_process(COMMAND rustup target add --toolchain ${Rust_TOOLCHAIN} wasm32-wasip1)
+# Parse Rust version and target from rust-toolchain.toml.
+file(READ "${CMAKE_CURRENT_SOURCE_DIR}/rust-toolchain.toml" RUST_TOOLCHAIN_FILE)
+if (NOT ${RUST_TOOLCHAIN_FILE} MATCHES [[channel *= *"([^\"]+)\"]])
+    message(FATAL_ERROR "Couldn't parse Rust toolchain version from file ${CMAKE_CURRENT_SOURCE_DIR}/rust-toolchain.toml")
+endif()
+set(Rust_TOOLCHAIN ${CMAKE_MATCH_1})
+
+if (NOT ${RUST_TOOLCHAIN_FILE} MATCHES [[targets = *\[ *"([^\"]+)]])
+    message(FATAL_ERROR "Couldn't parse Rust target from file ${CMAKE_CURRENT_SOURCE_DIR}/rust-toolchain.toml")
+endif()
+set(Rust_CARGO_TARGET ${CMAKE_MATCH_1})
+
+if (NOT Rust_VERSION VERSION_EQUAL ${Rust_TOOLCHAIN})
+    execute_process(COMMAND rustup toolchain install ${Rust_TOOLCHAIN})
+endif()
+execute_process(COMMAND rustup target add --toolchain ${Rust_TOOLCHAIN} ${Rust_CARGO_TARGET})
 
 CPMAddPackage("gh:corrosion-rs/corrosion@0.5.1")
 string(TOLOWER ${Rust_CARGO_HOST_ARCH} HOST_ARCH)
