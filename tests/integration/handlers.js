4d9fa6758943894681e4475d59de77c0c063b90f
Tomasz Andrzejak <andreiltd@gmail.com>
2025-01-16 19:09:42 +0100

    Add multiple blob streams integration test

--- tests/integration/handlers.js
+++ tests/integration/handlers.js
@@ -3,3 +3,4 @@ export { handler as performance } from './performance/performance.js';
 export { handler as crypto } from './crypto/crypto.js';
 export { handler as timers } from './timers/timers.js';
 export { handler as fetch } from './fetch/fetch.js';
+export { handler as file } from './file/file.js';
