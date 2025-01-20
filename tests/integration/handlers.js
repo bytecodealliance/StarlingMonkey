50fd0a7ea12ad570e81963f08d4d63bb91396ac2
Tomasz Andrzejak <andreiltd@gmail.com>
2025-01-20 14:52:39 +0100

    Rename file integration test to blob

--- tests/integration/handlers.js
+++ tests/integration/handlers.js
@@ -1,6 +1,6 @@
+export { handler as blob } from './blob/blob.js';
 export { handler as btoa } from './btoa/btoa.js';
 export { handler as performance } from './performance/performance.js';
 export { handler as crypto } from './crypto/crypto.js';
 export { handler as timers } from './timers/timers.js';
 export { handler as fetch } from './fetch/fetch.js';
-export { handler as file } from './file/file.js';
