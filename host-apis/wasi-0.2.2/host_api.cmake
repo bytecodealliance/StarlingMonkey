4306ff0cbe160ba64d1f8fdbf633c3791e2f0b6d
Till Schneidereit <till@tillschneidereit.net>
2025-01-21 16:12:16 +0100

    Deduplicate code between wasi-0.2.2 and wasi-0.2.0 host API implementations
    Signed-off-by: Till Schneidereit <till@tillschneidereit.net>

--- host-apis/wasi-0.2.2/host_api.cmake
+++ host-apis/wasi-0.2.2/host_api.cmake
@@ -1,19 +1,20 @@
+set(WASI_0_2_0 "${HOST_API}/../wasi-0.2.0")
+
 add_library(host_api STATIC
-        ${HOST_API}/host_api.cpp
-        ${HOST_API}/host_call.cpp
+        ${WASI_0_2_0}/host_api.cpp
+        ${WASI_0_2_0}/host_call.cpp
         ${HOST_API}/bindings/bindings.c
         ${HOST_API}/bindings/bindings_component_type.o
-        ${CMAKE_CURRENT_SOURCE_DIR}/include/host_api.h
 )
 
 target_link_libraries(host_api PRIVATE spidermonkey)
 target_include_directories(host_api PRIVATE include)
-target_include_directories(host_api PRIVATE ${HOST_API})
-target_include_directories(host_api PUBLIC ${HOST_API}/include)
+target_include_directories(host_api PRIVATE ${WASI_0_2_0})
+target_include_directories(host_api PUBLIC ${WASI_0_2_0}/include)
 
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     set(ADAPTER "debug")
 else()
     set(ADAPTER "release")
 endif()
-set(ADAPTER "${HOST_API}/preview1-adapter-${ADAPTER}/wasi_snapshot_preview1.wasm")
+set(ADAPTER "${WASI_0_2_0}/preview1-adapter-${ADAPTER}/wasi_snapshot_preview1.wasm")
