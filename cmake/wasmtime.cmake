5ed4e1faf4b2a6cd3e2cbde154eb064e77c1c991
Till Schneidereit <till@tillschneidereit.net>
2025-01-21 18:11:57 +0100

    deps: update to WASI@0.2.3 (#201)
    Same as with wasi-0.2.2, the actual API implementation is reused from wasi-0.2.0, but the bindings target 0.2.3.

--- cmake/wasmtime.cmake
+++ cmake/wasmtime.cmake
@@ -1,4 +1,4 @@
-set(WASMTIME_VERSION v26.0.0)
+set(WASMTIME_VERSION v29.0.0)
 set(WASMTIME_URL https://github.com/bytecodealliance/wasmtime/releases/download/${WASMTIME_VERSION}/wasmtime-${WASMTIME_VERSION}-${HOST_ARCH}-${HOST_OS}.tar.xz)
 CPMAddPackage(NAME wasmtime URL ${WASMTIME_URL} DOWNLOAD_ONLY TRUE)
 set(WASMTIME_DIR ${CPM_PACKAGE_wasmtime_SOURCE_DIR})
