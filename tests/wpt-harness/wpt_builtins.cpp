17313a6442035af394ee739b0fb680e3eb74259d
Till Schneidereit <till@tillschneidereit.net>
2025-02-12 19:01:53 +0100

    Fix loading of WPT META scripts (#209)
    WPT tests can load helper scripts using special comments at the top. These scripts need to be loaded as though using a `<script>` tag, which doesn't create a new lexical scope, and hence causes `let` and `const` bindings to be visible in the global scope.
    
    Previously, to simulate this behavior using `eval`, the WPT harness replaced `let` and `const` with `var`. While this worked surprisingly well, it doesn't *always* work.
    
    This commit instead introduces an `evalScript` function available to the harness, which evaluates in a way more closely matching that of script tags.
    
    Signed-off-by: Till Schneidereit <till@tillschneidereit.net>

--- tests/wpt-harness/wpt_builtins.cpp
+++ tests/wpt-harness/wpt_builtins.cpp
@@ -1,5 +1,8 @@
 #include "extension-api.h"
 
+#include <encode.h>
+#include <js/CompilationAndEvaluation.h>
+#include <js/SourceText.h>
 #include <url.h>
 #include <worker-location.h>
 
@@ -26,14 +29,36 @@ static bool baseURL_get(JSContext *cx, unsigned argc, JS::Value *vp) {
   return true;
 }
 
+/**
+ * Evaluate the given script in the global scope, without creating a new lexical scope.
+ * This is roughly equivalent to how `<script>` tags work in HTML, and hence how
+ * the WPT harness needs to load `META` scripts: otherwise, `let` and `const` bindings
+ * aren't visible to importing code, and the tests break.
+ */
+static bool evalScript(JSContext *cx, unsigned argc, JS::Value *vp) {
+  CallArgs args = CallArgsFromVp(argc, vp);
+  auto script = core::encode(cx, args.get(0));
+  if (!script) {
+    return false;
+  }
+  JS::SourceText<mozilla::Utf8Unit> source;
+  MOZ_RELEASE_ASSERT(source.init(cx, std::move(script.ptr), script.len));
+  JS::CompileOptions options(cx);
+  options.setNonSyntacticScope(true);
+  return Evaluate(cx, options, source, args.rval());
+}
+
 
 const JSPropertySpec properties[] = {
-  JS_PSGS("baseURL", baseURL_get, baseURL_set, JSPROP_ENUMERATE),
+  JS_PSGS("wpt_baseURL", baseURL_get, baseURL_set, JSPROP_ENUMERATE),
 JS_PS_END};
 
 namespace wpt_support {
 
 bool install(api::Engine* engine) {
+  if (!JS_DefineFunction(engine->cx(), engine->global(), "evalScript", evalScript, 1, 0)) {
+    return false;
+  }
   if (!JS_DefineProperties(engine->cx(), engine->global(), properties)) {
     return false;
   }
