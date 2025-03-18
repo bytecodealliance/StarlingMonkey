5590f40325bfad2f0a8ed5a55ee845e7fe9d2efc
Till Schneidereit <till@tillschneidereit.net>
2025-03-18 19:08:35 +0100

    Small CMake setup improvements (#229)
    This commit includes two kinds of changes:
    1. It makes various tools (binaryen, wasm-tools, wasmtime, weval, and wizer) that are installed during configuration available to embeddings by moving the variables into the CMake variables cache
    2. It changes the OpenSSL configuration to ignore patches that are applied multiple times, making the configuration more robust
    
    The latter change is needed in situations where an embedding moves to a different version of StarlingMonkey without deleting the build dir. That causes the OpenSSL config to be reapplied, while the old, already patched, files are kept around.

--- cmake/binaryen.cmake
+++ cmake/binaryen.cmake
@@ -8,7 +8,7 @@ endif()
 set(BINARYEN_URL https://github.com/WebAssembly/binaryen/releases/download/version_${BINARYEN_VERSION}/binaryen-version_${BINARYEN_VERSION}-${BINARYEN_ARCH}-${HOST_OS}.tar.gz)
 CPMAddPackage(NAME binaryen URL ${BINARYEN_URL} DOWNLOAD_ONLY TRUE)
 set(BINARYEN_DIR ${CPM_PACKAGE_binaryen_SOURCE_DIR}/bin)
-set(WASM_OPT ${BINARYEN_DIR}/wasm-opt)
+set(WASM_OPT ${BINARYEN_DIR}/wasm-opt CACHE FILEPATH "Path to wasm-opt binary")
 
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/scripts/cxx-wrapper.in ${CMAKE_BINARY_DIR}/cxx-wrapper)
 set(CMAKE_CXX_COMPILER "${CMAKE_BINARY_DIR}/cxx-wrapper")
