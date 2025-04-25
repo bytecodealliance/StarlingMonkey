b1ef740c7ff1f4b79941b8e32218a4a1b8e9a42c
Tomasz Andrzejak <andreiltd@gmail.com>
2025-04-25 11:28:01 +0200

    Add clang-tidy and clang-tidy-fix targets

---
+++ cmake/lint.cmake
@@ -0,0 +1,28 @@
+set(CLANG_TIDY ${WASI_SDK_PREFIX}/bin/clang-tidy)
+set(RUN_CLANG_TIDY ${WASI_SDK_PREFIX}/bin/run-clang-tidy)
+set(CLANG_APPLY_REPLACEMENTS ${WASI_SDK_PREFIX}/bin/clang-apply-replacements)
+set(WASI_SYSROOT ${WASI_SDK_PREFIX}/share/wasi-sysroot)
+
+add_custom_target(clang-tidy
+  COMMAND ${RUN_CLANG_TIDY}
+          -p=${CMAKE_BINARY_DIR}
+          -clang-tidy-binary=${CLANG_TIDY}
+          -extra-arg=--sysroot=${WASI_SYSROOT}
+          -config-file=${CMAKE_SOURCE_DIR}/.clang-tidy
+          ${CMAKE_SOURCE_DIR}/builtins
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  COMMENT "Running clang‑tidy over the codebase"
+  VERBATIM)
+
+add_custom_target(clang-tidy-fix
+  COMMAND ${RUN_CLANG_TIDY}
+          -p=${CMAKE_BINARY_DIR}
+          -clang-tidy-binary=${CLANG_TIDY}
+          -clang-apply-replacements-binary=${CLANG_APPLY_REPLACEMENTS}
+          -extra-arg=--sysroot=${WASI_SYSROOT}
+          -config-file=${CMAKE_SOURCE_DIR}/.clang-tidy
+          -fix
+          ${CMAKE_SOURCE_DIR}/builtins
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  COMMENT "Running clang‑tidy over the codebase and applying fixes"
+  VERBATIM)
