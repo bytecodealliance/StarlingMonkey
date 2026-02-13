4f7257856ac2d4ccb3e85c8a93f8dac78571d147
Ivan Krasilnikov <ivank@ivank.me>
2026-02-13 16:19:07 +0800

    fix: aarch64->arm64 in wasi-sdk download url

--- cmake/wasi-sdk.cmake
+++ cmake/wasi-sdk.cmake
@@ -1,6 +1,7 @@
 set(WASI_SDK_VERSION 25 CACHE STRING "Version of wasi-sdk to use" FORCE)
 
-set(WASI_SDK_URL "https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_SDK_VERSION}/wasi-sdk-${WASI_SDK_VERSION}.0-${HOST_CPU}-${HOST_OS}.tar.gz")
+string(REPLACE "aarch64" "arm64" WASI_SDK_ARCH ${HOST_CPU})
+set(WASI_SDK_URL "https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_SDK_VERSION}/wasi-sdk-${WASI_SDK_VERSION}.0-${WASI_SDK_ARCH}-${HOST_OS}.tar.gz")
 CPMAddPackage(NAME wasi-sdk URL ${WASI_SDK_URL})
 set(WASI_SDK_PREFIX ${CPM_PACKAGE_wasi-sdk_SOURCE_DIR})
 set(CMAKE_TOOLCHAIN_FILE ${WASI_SDK_PREFIX}/share/cmake/wasi-sdk.cmake)
